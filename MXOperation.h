b630816ddfe25ead34a8e71b9a1f31995ad56c92
IOS_HMX <ios_hmx@IOS-HMXdeMacBook-Pro.local>
2018-05-11 17:12:21 +0800

    2

--- MXOperation.h
+++ MXOperation.h
@@ -1,7 +1,7 @@
 //
 //  MXOperation.h
 //  HMXStockInfo
-//  master 1
+//  master 1 2
 //  Created by IOS_HMX on 15/6/29.
 //  Copyright (c) 2015年 IOS_HMX. All rights reserved.
 // 11111 3333 444 555  66
